edb1220e035d749bcdc2dde109b166b7fa2f99d4
Piotr Husiatynski <piotr.husiatynski@starmeo.com>
2012-08-14 10:49:03 +0200

    pagination css updated

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,6 @@ import os
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_PATH = os.path.join(PROJECT_DIR, 'storage.sqlite3.db')
 
-PER_PAGE_LIMIT = 20
+PER_PAGE_LIMIT = 25
 
 VOTE_COOKIE_KEY = '_s'
